c3cabb9c7f5cefa790ffbcf8a85d1b48138b6222
Andrew Dunstall <andy@anity.io>
2021-06-16 17:54:59 +0100

    Add output img

--- fake_package/test_mock_2.py
+++ fake_package/test_mock_2.py
@@ -1,5 +1,6 @@
 import time
 import unittest
+import sys
 
 
 class TestMock2(unittest.TestCase):
@@ -9,9 +10,12 @@ class TestMock2(unittest.TestCase):
 
     def test_2(self):
         time.sleep(0.2)
+        # Syntax error to fail test.
         skdlfj
         self.assertEqual(2 + 2, 4)
 
     def test_3(self):
         time.sleep(0.3)
+        print('my stdout')
+        print('my stderr', file=sys.stderr)
         self.assertEqual(3 + 3, 6)
